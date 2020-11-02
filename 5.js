15529d302d6a0be5e3f332425cc7910a7d5c6183
NastiaYurova <iamnassttya@gmail.com>
2020-11-03 01:02:20 +0200

    Initial commit

---
+++ 5.js
@@ -0,0 +1,480 @@
+// T1
+//
+// let str1 = "hello";
+// let str2 = "owu";
+// let str3 = "com";
+// let str4 = "ua";
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+// let boolean1 = true;
+// let boolean2 = false;
+//
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+//
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+//
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+//
+// console.log(str4);
+// alert(str4);
+// document.write(str4);
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// console.log(boolean1);
+// alert(boolean1);
+// document.write(boolean1);
+//
+// console.log(boolean2);
+// alert(boolean2);
+// document.write(boolean2);
+
+
+// T2
+//
+// let str1 = "Hello!";
+// let str2 = "we";
+// let str3 = "are";
+// let str4 = "u";
+// let num1 = 5;
+// let num2 = 100;
+// let num3 = -3;
+// let num4 = 123456;
+// let num5 = 3.1433;
+// let num6 = "okay";
+// let num7 = 25000;
+// let boolean1 = true;
+// let boolean2 = false;
+//
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+//
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+//
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+//
+// console.log(str4);
+// alert(str4);
+// document.write(str4);
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// console.log(boolean1);
+// alert(boolean1);
+// document.write(boolean1);
+//
+// console.log(boolean2);
+// alert(boolean2);
+// document.write(boolean2);
+
+
+// T3
+//
+// const num1 = 33;
+// const num2 = 52;
+// const num3 = 6;
+//
+// const str1 = "Hello!";
+// const str2 = "How are u?";
+// const str3 = "Bye!"
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+//
+// console.log(str2);
+// alert(str2);
+// document.write(str2);
+//
+// console.log(str3);
+// alert(str3);
+// document.write(str3);
+
+
+
+// T4
+//
+// let firsName = prompt("Enter your first name");
+// let lastName = prompt("Enter yor last name");
+// let surname = prompt("Enter your surname");
+//
+// console.log(firsName);
+// console.log(lastName);
+// console.log(surname);
+//
+// document.write(firsName + " ");
+// document.write(lastName + " ");
+// document.write(surname + " ");
+//
+// alert(firsName);
+// alert(lastName);
+// alert(surname);
+
+
+
+// T5
+//
+// let firsName = prompt("Enter your first name");
+// let lastName = prompt("Enter yor last name");
+// let surname = prompt("Enter your surname");
+//
+// let person = firsName + " " + lastName + " " + surname;
+
+
+
+// T6
+//
+// let firsNameM = prompt("Enter mom first name");
+// let lastNameM = prompt("Enter mom last name");
+// let surnameM = prompt("Enter mom surname");
+//
+// let firsNameD = prompt("Enter dad first name");
+// let lastNameD = prompt("Enter dad last name");
+// let surnameD = prompt("Enter dad surname");
+//
+// let firsNameB = prompt("Enter brother first name");
+// let lastNameB = prompt("Enter brother last name");
+// let surnameB = prompt("Enter brother surname");
+//
+// let firsNameI = prompt("Enter I first name");
+// let lastNameI = prompt("Enter I last name");
+// let surnameI = prompt("Enter I surname");
+//
+// let mom = firsNameM + " " + lastNameM + " " + surnameM;
+// let dad = firsNameD + " " + lastNameD + " " + surnameD;
+// let brother = firsNameB + " " + lastNameB + " " + surnameB;
+// let I = firsNameI + " " + lastNameI + " " + surnameI;
+//
+// console.log(mom);
+// document.write(mom + "<br/>");
+// alert(mom);
+//
+// console.log(dad);
+// document.write(dad + "<br/>");
+// alert(dad);
+//
+// console.log(brother);
+// document.write(brother + "<br/>");
+// alert(brother);
+//
+// console.log(I);
+// document.write(I + "<br/>");
+// alert(I);
+
+
+
+// T7
+//
+// let num1 = +prompt("Enter number one");
+// let num2 = +prompt("Enter number two");
+// let num3 = +prompt("Enter number three");
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+
+
+// T8
+//
+// let num1 = parseInt(prompt("Enter number one"));
+// let num2 = parseInt(prompt("Enter number two"));
+// let num3 = parseInt(prompt("Enter number three"));
+// let num4 = parseInt(prompt("Enter number four"));
+//
+// let result = num1 + num2 + num3 + num4;
+//
+// console.log(result);
+
+
+
+// T9
+//
+// let num1 = parseFloat(prompt("Enter number one"));
+// let num2 = parseFloat(prompt("Enter number two"));
+// let num3 = parseFloat(prompt("Enter number three"));
+//
+// let result = num1 + num2 + num3;
+//
+// console.log(result);
+
+
+
+// T10
+//
+// let num1 = parseFloat(prompt("Enter number one"));
+// let num2 = parseFloat(prompt("Enter number two"));
+// let num3 = parseFloat(prompt("Enter number three"));
+//
+// num1 = Math.round(num1);
+// num2 = Math.round(num2);
+// num3 = Math.round(num3);
+//
+// let result = num1 + num2 + num3;
+//
+// console.log(result);
+
+
+
+// T11
+//
+// let num1 = parseInt(prompt("Enter number one"));
+// let num2 = parseInt(prompt("Enter number two"));
+//
+// let result = Math.pow(num1 , num2);
+
+
+
+// T12
+//
+// let a = 100;
+// let b = "100";
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+
+// T13
+//
+// let a = 5 < 6;
+// let b = 5 > 6
+// let c = 5 == 6;
+// let d = 5 >= 6;
+// let e = 10 == 10;
+// let f = 10 >= 10;
+// let g = 10 > 10;
+// let h = 10 < 10;
+// let i = 10 <= 10;
+// let j = 123 === '123';
+// let k = 123 == '123';
+
+
+
+// DODATOK
+//
+//console.log(132 > 100 && 45 < 12 ); - false
+//console.log(34 > 33 && 23 < 90 ); - true
+//console.log(99 > 100 && 45 > 12 ); - false
+//console.log(132 > 100 || 45 < 12 ); - true
+//console.log(111 > 11 || 45 < 111 ); - true
+//console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); - true
+//console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); - true
+//console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); - false
+//console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); - true
+//console.log(!!'-1'); - true ???
+//console.log(!!-1); - true ???
+//console.log(!!'0'); - true ???
+//console.log(!!'null'); - true ???
+//console.log(!!'undefined'); - true ???
+//console.log(!!(3/'owu')); - false ???
+//console.log((111 > 11 || 45 < 111) ||  !!'0'); - true ???
+//console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); - false ???
+
+
+
+
+
+
+//CLASS//
+// Task 1
+//var txt = "Привет";
+//var num= 123;
+//var flag = true;
+//var str = false;
+//alert(typeof txt);
+//alert(typeof num);
+//alert(typeof str);
+
+//Task 2
+// let a1 = 5 + 3
+// let a2 = 5 - 3
+// let a3 = 5 * 3
+// let a4 = 5 / 3
+// let a5 = 5 % 3
+//
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+//Task 3
+// let a6 = 5 % 3
+// let a7 = 3 % 5
+// let a8 = 5 + '3'
+// let a9 = '5' - 3
+// let a10 = 75 + 'кг'
+//
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+
+//Task 4
+// let height = 23
+// let width = 10
+// s = height * width
+// console.log(s);
+
+
+//Task 5
+// heightC = 10
+// dC = 4
+// v = Math.PI * heightC * dC
+// console.log(v)
+
+//Task 6
+// let n = 3
+// let m = 4
+// k = Math.pow(3, 2) * Math.pow(4,2)
+// console.log(k)
+
+//Task 7
+// let str = "Hello World"
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+//Task 8
+// let a = "Nastya Yurova\n"
+// let b = '19\n'
+// let c = "reading books\n"
+// alert(a)
+// alert(b)
+// alert(c)
+
+//Task 9
+// let str1 = "Кто"
+// let str2 = "ты"
+// let str3 = "такой?"
+// let concatenation = str1 + str2 + str3
+// document.write(concatenation)
+
+//Task 10
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+//205
+// 15
+// 40
+// 10
+
+//Task 11
+// parseInt("3.14")
+// parseInt("-7.875")
+// parseInt("435")
+// parseInt("Вася")
+// console.log(parseInt(3.14))
+// console.log(parseInt(-7.875))
+// console.log(parseInt(435))
+// console.log(parseInt("Вася"))
+ //3
+//-7
+//435
+//NaN
+
+// Task 12
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+//ho-ho
+
+// Task 13
+// let str = prompt("5 + 3" , "8" )
+// alert(str)
+//8
+
+//Task 14
+// let firstname = prompt("Enter your first name")
+// let lastname = prompt("Enter your last name")
+// let age = prompt("Enter your age")
+// alert(" Доброго вечера" + " " + firstname + " " + lastname+ " " + "мои поздравления что вам" + " " + age)
